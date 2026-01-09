197b6c89bfc16cbcc964f07f8061a46fb33de886
Wadad Parker <wadadparker5@gmail.com>
2026-01-09 19:15:22 +0530

    refactor: profile header

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
 import ArrowDown from "./components/icons/ArrowDown"
 import Navbar from "./components/main/Navbar"
-import logo from "./assets/logo.png"
-import ArrowDownWhite from "./components/icons/ArrowDownWhite"
+import ProfileHeader from "./components/sidebar/ProfileHeader"
+
 
 function App() {
 
   return (
       <div className="bg-background-white flex h-screen w-screen overflow-hidden">
         <div className=" w-56 bg-primary py-4 px-2">
-          <header className='flex space-x-3 px-2.25'>
-            <img src={logo} alt='user profile' width={39} height={39} className=' rounded object-cover' />
-            <aside className='grow space-y-1'>
-                <h1 className='text-white font-medium text-[15px]'>Nishyan</h1>
-                <p className=' text-[13px] leading-4 text-white/80 underline hover:cursor-pointer w-fit'>Visit store</p>
-            </aside>
-            <ArrowDownWhite />
-          </header>
+          <ProfileHeader />
         </div>
         <div className="grow ">
           <Navbar />
--- src/components/sidebar/ProfileHeader.tsx
+++ src/components/sidebar/ProfileHeader.tsx
@@ -1,7 +1,16 @@
+import logo from "../../assets/logo.png"
+import ArrowDownWhite from "../icons/ArrowDownWhite" 
 
 const ProfileHeader = () => {
   return (
-    <div>ProfileHeader</div>
+    <header className='flex space-x-3 px-2.25'>
+    <img src={logo} alt='user profile' width={39} height={39} className=' rounded object-cover' />
+    <aside className='grow space-y-1'>
+        <h1 className='text-white font-medium text-[15px]'>Nishyan</h1>
+        <p className=' text-[13px] leading-4 text-white/80 underline hover:cursor-pointer w-fit'>Visit store</p>
+    </aside>
+    <ArrowDownWhite />
+    </header>
   )
 }
 

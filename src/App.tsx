a29665cdbd6336ef768f232c63ea3b6b0bf6de94
Wadad Parker <wadadparker5@gmail.com>
2026-01-09 22:34:04 +0530

    style: amount card

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import ArrowDown from "./components/icons/ArrowDown"
+import ArrowRight from "./components/icons/ArrowRight"
+import QuestionMark from "./components/icons/QuestionMark"
 import Navbar from "./components/main/Navbar"
+import NavItems from "./components/sidebar/NavItems"
 import ProfileHeader from "./components/sidebar/ProfileHeader"
 
 
@@ -9,6 +12,7 @@ function App() {
       <div className="bg-background-white flex h-screen w-screen overflow-hidden">
         <div className=" w-56 bg-primary py-4 px-2">
           <ProfileHeader />
+          <NavItems />
         </div>
         <div className="grow ">
           <Navbar />
@@ -23,6 +27,22 @@ function App() {
                 <ArrowDown />
               </aside>
             </section>
+            <ul className="flex">
+              <li className="rounded-md bg-white p-4 space-y-2">
+                <header className="flex items-center gap-x-2">
+                  <h1 className="text-text">Amount Pending</h1>
+                  <QuestionMark />
+                </header>
+
+                <aside className="flex justify-between">
+                  <h1 className=" text-3xl font-medium">₹23,72.20</h1>
+                  <button className="flex items-center underline gap-x-2.5">
+                    <p className="mb-1">13 order</p>
+                    <ArrowRight />
+                  </button>
+                </aside>
+              </li>
+            </ul>
           </main>
         </div>
       </div>

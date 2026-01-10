841562513d2663795af4963e3e990c0a435ba1c3
Wadad Parker <wadadparker5@gmail.com>
2026-01-10 12:46:02 +0530

    feat: center a div

--- src/Pages/Layout.tsx
+++ src/Pages/Layout.tsx
@@ -1,14 +1,10 @@
 
 const Layout = () => {
   return (
-    <div className="text-center">
-        <div className="bg-red-100 inline h-10">div</div>
-        <h1 className="bg-blue-100 inline h-10">h1</h1>
-        <p className="bg-green-100 inline h-10">p</p>
-        <span className="bg-amber-100 block h-10">span {"(height applies for block)"}</span>
-        <b className="bg-violet-100 inline-block h-10">bold (behaves inline & height applies)</b>
-        <li className="bg-orange-100 ">li</li>
-        <small className="bg-gray-100 h-10">small (height does not apply for inline)</small>
+    <div className="flex justify-center items-center h-screen bg-blue-100 ">
+       <section className="bg-red-300 p-4 text-3xl">
+        Center
+       </section>
     </div>
   )
 }
